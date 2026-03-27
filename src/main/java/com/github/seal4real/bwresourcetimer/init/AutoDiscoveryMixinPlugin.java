2d2e31d145352dc221c10ada1ad14d83b4fd49db
GitHub Action <action@github.com>
2026-03-27 22:33:55 +0000

    Initialize template repository proper
    Set package name to com.github.seal4real.bwresourcetimer
    Set project name to bw-resource-timer
    Set modid to bwresourcetimer

--- src/main/java/com/github/seal4real/bwresourcetimer/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/seal4real/bwresourcetimer/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.seal4real.bwresourcetimer.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
