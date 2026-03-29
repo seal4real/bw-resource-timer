acb6b2f27a84b4f3889bfb5e3e3d782be63526f2
ostarkey-unimelb <oliver.starkey@student.unimelb.edu.au>
2026-03-29 16:31:54 +1100

    Add locraw send

--- src/main/java/com/github/seal4real/bwresourcetimer/events/BwEventListener.java
+++ src/main/java/com/github/seal4real/bwresourcetimer/events/BwEventListener.java
@@ -1,15 +1,35 @@
 package com.github.seal4real.bwresourcetimer.events;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class BwEventListener {
-    int chatCount = 0;
+    int ticks = -1;
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        ticks = 20; // wait 1 second (20 ticks) before sending /locraw
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.START) return;
+        if (ticks < 0) return;
+
+        ticks--;
+        if (ticks == 0) {
+            Minecraft.getMinecraft().thePlayer.sendChatMessage("/locraw");
+            ticks = -1;
+        }
+    }
 
     @SubscribeEvent
     public void chat(ClientChatReceivedEvent event) {
-        chatCount++;
-        System.out.println(chatCount);
+        String msg = event.message.getUnformattedText();
+        System.out.println("CHAT: " + msg);
     }
 
 }
